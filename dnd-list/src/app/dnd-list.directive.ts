2ca52d35000c017ed9c22a02dd478f8bb0250ad7
Zyllio <96659995+zyllio@users.noreply.github.com>
2023-10-21 18:14:30 +0200

    -

--- dnd-list/src/app/dnd-list.directive.ts
+++ dnd-list/src/app/dnd-list.directive.ts
@@ -3,6 +3,7 @@ import { Directive, ElementRef, Output, EventEmitter, Input, OnInit, OnDestroy, 
 import { fromEvent } from 'rxjs';
 import { map, takeUntil, finalize, mergeMap, filter } from 'rxjs/operators';
 
+const shift = 60
 
 @Directive({
   selector: '[app-dnd-list]'
@@ -65,7 +66,7 @@ export class DndListDirective implements AfterViewInit {
           filter((mm: MouseEvent) => mm.clientX !== startX || mm.clientY !== startY),
           map((mm: MouseEvent) => {
 
-            itemElement.style.transition = 'none';
+            itemElement.style.transition = '';
             itemElement.style.zIndex = '1000';
             itemElement.style.pointerEvents = 'none'
             itemElement.classList.add('dragging')
@@ -86,102 +87,99 @@ export class DndListDirective implements AfterViewInit {
 
               if (this.intersectRect(itemElement, sibling)) {
                 interectSibling = sibling
+
+                if (mm.movementY === 1) {
+                  this.shiftUp(sibling)
+                } else {
+                  this.shiftDown(sibling)
+                }
               }
-
-            })
-
-            // console.log('interectSibling', interectSibling)
-
-            const intersectIndex = this.getItemIndex(listElement, interectSibling!)
-
-            const oldPosition = newPosition
-
-            console.log('intersectIndex', intersectIndex)
-
-            siblings.forEach((sibling, siblingIndex) => {
-
-              // Don't use a filter here
-              if (sibling === itemElement) {
-                return;
-              }
-
-              if (siblingIndex === intersectIndex) {
-                newPosition = siblingIndex
-              }
-            })
-
-            // Move items
-            siblings.forEach((sibling, siblingIndex) => {
-
-              // Don't use a filter here
-              if (sibling === itemElement) {
-                return;
-              }
-
-              if(siblingIndex === newPosition) {
-
-                const top2 = (top < 0) ? '60' : '-60'
-
-                sibling.style.transform = `translateY(${top2}px)`
-              }
-
-              // if(newPosition > oldPosition) {
-
-              // }
-
-
-
             })
 
 
           }),
-      takeUntil(mouseup),
-      finalize(() => {
+          takeUntil(mouseup),
+          finalize(() => {
 
-        itemElement.style.pointerEvents = 'all'
+            itemElement.style.pointerEvents = 'all'
 
-        itemElement.classList.remove('dragging')
+            itemElement.classList.remove('dragging')
 
-        this.moved.emit({ item: itemIndex, position: newPosition })
+            this.moved.emit({ item: itemIndex, position: newPosition })
 
-        siblings.forEach((sibling, siblingIndex) => {
-          sibling.style.transform = `none`
-          sibling.style.transition = `none`
-        })
-      }));
-  })
+            siblings.forEach((sibling, siblingIndex) => {
+              sibling.style.transform = ``
+              sibling.style.transition = ``
+            })
+          }));
+      })
     );
 
     mousedrag.subscribe(() => {
-});
+    });
+  }
+
+  shiftUp(element: HTMLElement) {
+
+    const transform = element.style.transform
+
+    if (transform === '') {
+
+      element.style.transform = `translateY(-${shift}px)`
+
+    } else if (transform === `translateY(${shift}px)`) {
+
+      element.style.transform = ''
+    }
+
+    console.log("transform ", transform, element.style.transform);
+
+  }
+
+  shiftDown(element: HTMLElement) {
+
+    const transform = element.style.transform
+
+    if (transform === '') {
+
+      element.style.transform = `translateY(${shift}px)`
+
+    } else if (transform === `translateY(-${shift}px)`) {
+
+      element.style.transform = ''
+    }
+
+    console.log("transform ", transform, element.style.transform);
   }
 
   private intersectRect(item1: HTMLElement, item2: HTMLElement) {
 
-  const proxyRect = item1.getBoundingClientRect();
-  const dropRect = item2.getBoundingClientRect();
+    const proxyRect = item1.getBoundingClientRect();
+    const dropRect = item2.getBoundingClientRect();
 
-  return Math.max(proxyRect.left, dropRect.left) < Math.min(proxyRect.left + proxyRect.width, dropRect.left + dropRect.width) &&
-    Math.max(proxyRect.top, dropRect.top) < Math.min(proxyRect.top + proxyRect.height, dropRect.top + dropRect.height);
-}
+    const margin = 15
+
+    return Math.max(proxyRect.left, dropRect.left) < Math.min(proxyRect.left + proxyRect.width, dropRect.left + dropRect.width) &&
+      Math.max(proxyRect.top, dropRect.top + margin) < Math.min(proxyRect.top + proxyRect.height - margin, dropRect.top + dropRect.height - margin);
+  }
 
   private restrictToBoundaries(array: any[], index: number): number {
 
-  let newIndex = (index < 0) ? 0 : index;
-  newIndex = (newIndex > array.length - 1) ? array.length - 1 : newIndex;
+    let newIndex = (index < 0) ? 0 : index;
+    newIndex = (newIndex > array.length - 1) ? array.length - 1 : newIndex;
 
-  return newIndex;
-}
+    return newIndex;
+  }
 
   private getSiblings(listElement: HTMLElement) {
-  return Array.from(listElement.children) as HTMLElement[];
-}
+    return Array.from(listElement.children) as HTMLElement[];
+  }
 
   private getItemIndex(listElement: HTMLElement, itemElement: HTMLElement) {
 
-  const siblings = this.getSiblings(listElement)
+    const siblings = this.getSiblings(listElement)
 
-  return siblings.findIndex(c => c === itemElement)
-}
+    return siblings.findIndex(c => c === itemElement)
+  }
 
 }
